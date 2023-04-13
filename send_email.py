b64dca35c5b159389abbc1ae0e279ad34bb2a643
Stan <dimitrov.s.dev@gmail.com>
2023-04-13 13:46:03 +0300

    send_email added with os.getenv

---
+++ send_email.py
@@ -0,0 +1,16 @@
+import smtplib
+import ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "dimitrov.s.dev@gmail.com"
+    password = os.getenv("PASSWORD")
+    receiver = "dimitrov.s.dev@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
